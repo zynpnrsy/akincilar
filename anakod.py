de05507e94937e39fae6668fb9495bb2cb71d251
tuannaakarsu <95305483+tuannaakarsu@users.noreply.github.com>
2023-08-06 16:15:46 +0300

    Update anakod.py

--- anakod.py
+++ anakod.py
@@ -4,6 +4,7 @@ cam = cv2.VideoCapture(0)
                     
 while cam.isOpened():
     ret, frame = cam.read()
+    frame = cv2.flip(frame,1)
 
     cv2.imshow('kamera',frame)
     k=cv2.waitKey(1)& 0xFF
